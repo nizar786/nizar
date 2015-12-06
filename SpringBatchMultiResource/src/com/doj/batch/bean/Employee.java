13f94192dcfc63c072dce8d8cf19a5437f696509
nizarhussain <nizar_computer786@outlook.com>
2015-12-05 23:46:30 -0600

    project

---
+++ SpringBatchMultiResource/src/com/doj/batch/bean/Employee.java
@@ -0,0 +1,80 @@
+package com.doj.batch.bean;
+
+import javax.xml.bind.annotation.XmlAccessOrder;
+import javax.xml.bind.annotation.XmlAccessorOrder;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Dinesh Rajput
+ *
+ */
+@XmlRootElement(name="employee")
+@XmlAccessorOrder(XmlAccessOrder.UNDEFINED)
+public class Employee {
+	private int empid;
+	private String name;
+	private int age;
+	private float salary;
+	private String address;
+	/**
+	 * @return the empid
+	 */
+	public int getEmpid() {
+		return empid;
+	}
+	/**
+	 * @param empid the empid to set
+	 */
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	/**
+	 * @return the salary
+	 */
+	public float getSalary() {
+		return salary;
+	}
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
